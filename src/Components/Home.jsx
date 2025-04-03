fec6f786ad76675ffde4026dd385d94c799fee11
Billy-Ugalde <billyugalde2004@gmail.com>
2025-04-02 23:19:21 -0600

    Lab 3

---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import WelcomeWidget from "./WelcomeWidget";
+
+
+export default function Home({email}) {
+
+    return (
+        <>
+            <h1>Your at Home</h1>
+            <WelcomeWidget email={email} />
+        </>
+    );
+
+
+}
---
+++ src/Components/WelcomeWidget.jsx
@@ -0,0 +1,13 @@
+
+
+export default function  WelcomeWidget({email}) {
+
+        return (
+
+            <>
+                <h2>Welcome to our {email}</h2> 
+            </>
+
+
+        )
+}
